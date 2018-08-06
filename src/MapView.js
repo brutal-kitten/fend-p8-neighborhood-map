8166692e56204fd2c2fc358c896313e3ec2fe5b4
brutal-kitten <olgaprudyus@gmail.com>
2018-08-06 18:49:28 +0200

    feat: add 6 markers that displaying by default

--- src/MapView.js
+++ src/MapView.js
@@ -4,6 +4,7 @@ import scriptLoader from 'react-async-script-loader'
 
 class MapView extends Component {
 
+
   componentWillReceiveProps({isScriptLoadSucceed}){
     if(isScriptLoadSucceed) {
       this.initMap();
@@ -17,8 +18,55 @@ class MapView extends Component {
           center: {lat: 52.232658, lng: 21.004934},
           zoom: 13
         });
-  }
 
+    var locations = [
+      {title: "Teatr Dramatyczny" , location: {lat: 52.231672, lng: 21.008181}},
+      {title: "Roma Musical Theater" , location: {lat: 52.227561, lng: 21.008323}},
+      {title: "Kwadrat Theater" , location: {lat: 52.235969, lng: 21.008606}},
+      {title: "Och-Teatr" , location: {lat: 52.214326, lng: 20.980349}},
+      {title: "Capitol Theater" , location: {lat: 52.241328, lng: 21.003365}},
+      {title: "Polish National Opera" , location: {lat: 52.243530, lng: 21.010739}}
+    ]
+
+    let markers = [];
+
+    let largeInfoWindow = new window.google.maps.InfoWindow();
+    var bounds = new window.google.maps.LatLngBounds();
+
+    function populateInfoWindow(marker, infowindow) {
+
+      if(infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '<div>');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick', function(){
+          infowindow.setMarker = null;
+        });
+      }
+    }
+
+    locations.forEach((item) => {
+      let position = item.location;
+      let title = item.title;
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: item.title
+      });
+
+      markers.push(marker);
+
+      bounds.extend(marker.position);
+
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow);
+      });
+    });
+
+    map.fitBounds(bounds);
+  }
 
   render() {
     return (
