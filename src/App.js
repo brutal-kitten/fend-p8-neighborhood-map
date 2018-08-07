e4dc26608c2b823acc9d17d5aa7a5e40fe8a20f1
brutal-kitten <olgaprudyus@gmail.com>
2018-08-07 17:03:46 +0200

    fix: markers are shown after loading the page

--- src/App.js
+++ src/App.js
@@ -28,6 +28,52 @@ class App extends Component {
     }
   }
 
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: 52.232658, lng: 21.004934},
+          zoom: 13
+        });
+
+    let markers = [];
+
+    let largeInfoWindow = new window.google.maps.InfoWindow();
+    var bounds = new window.google.maps.LatLngBounds();
+
+    function populateInfoWindow(marker, infowindow) {
+
+      if(infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '<div>');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick', function(){
+          infowindow.setMarker = null;
+        });
+      }
+    }
+
+      this.state.locations.forEach((item) => {
+      let position = {lat: item.location.lat, lng: item.location.lng};
+      let title = item.name;
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: item.id
+      });
+
+      markers.push(marker);
+
+      bounds.extend(marker.position);
+
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow);
+      });
+    });
+
+    map.fitBounds(bounds);
+  }
+
 
   render() {
     return (
@@ -44,8 +90,8 @@ class App extends Component {
             )}/>
             <Route exact path="/" render={() => (
               <MapView
-               locations={this.state.locations}
-               google={window.google}/>
+               initMap={this.initMap}
+               />
             )}/>
           </div>)
       }
--- src/MapView.js
+++ src/MapView.js
@@ -6,64 +6,16 @@ import PropTypes from 'prop-types'
 
 class MapView extends Component {
 
-  static propTypes = {
-    locations: PropTypes.array.isRequired,
-    google: PropTypes.object.isRequired
-  }
 
   componentWillReceiveProps({isScriptLoadSucceed}){
     if(isScriptLoadSucceed) {
-      this.initMap();
+      this.props.initMap();
     } else {
       alert("script not loaded");
     }
   }
 
-  initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 52.232658, lng: 21.004934},
-          zoom: 13
-        });
 
-    let markers = [];
-
-    let largeInfoWindow = new window.google.maps.InfoWindow();
-    var bounds = new window.google.maps.LatLngBounds();
-
-    function populateInfoWindow(marker, infowindow) {
-
-      if(infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '<div>');
-        infowindow.open(map, marker);
-        infowindow.addListener('closeclick', function(){
-          infowindow.setMarker = null;
-        });
-      }
-    }
-
-      this.props.locations.forEach((item) => {
-      let position = {lat: item.location.lat, lng: item.location.lng};
-      let title = item.name;
-      let marker = new window.google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        animation: window.google.maps.Animation.DROP,
-        id: item.id
-      });
-
-      markers.push(marker);
-
-      bounds.extend(marker.position);
-
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfoWindow);
-      });
-    });
-
-    map.fitBounds(bounds);
-  }
 
   render() {
     return (
