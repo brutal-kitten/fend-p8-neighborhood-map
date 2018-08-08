36a8f3fa6f2444b4d5c88553d5e8c0d4b2f70b42
brutal-kitten <olgaprudyus@gmail.com>
2018-08-08 16:42:24 +0200

    feat: add list of markers on map

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -7,11 +7,22 @@ export class MapContainer extends Component {
   render() {
     return (
       <div className="map" role="application">
-        <Map google={this.props.google} zoom={13}>
-          <Marker
-            onClick={this.onMarkerClick}
-            name={"current location"}
-          />
+        <Map
+          google={this.props.google}
+          zoom={13}
+          initialCenter={{lat: 52.232658, lng: 21.004934}}
+          >
+          {this.props.state.locations.map((place) => (
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={place.name}
+              position={{lat: place.location.lat, lng: place.location.lng}}
+              animation={this.props.google.maps.Animation.DROP}
+              key={place.id}
+            />
+          ))
+          }
           <InfoWindow
             onClose={this.onInfoWindowClose}>
             <div>
